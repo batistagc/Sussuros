69051a95d1d86b1c3a1a5843509a2bc0774453aa
VitorGK <vitorgk1@gmail.com>
2021-09-16 18:04:11 -0300

    Check if headphone is connected

--- Oregano/ViewControllers/MenuViewController.swift
+++ Oregano/ViewControllers/MenuViewController.swift
@@ -1,24 +1,28 @@
-//
-//  MenuViewController.swift
-//  Oregano
-//
-//  Created by Gabriel Batista Cristiano on 14/09/21.
-//
-
 import UIKit
 import SpriteKit
+import AVFoundation
 
 class MenuViewController: UIViewController {
     
     open override func viewDidLoad() {
         super.viewDidLoad()
         if let view = self.view as! SKView? {
-            let scene = MenuScene(size: view.bounds.size)
-            scene.scaleMode = .resizeFill
-            
-            view.presentScene(scene)
+            if isHeadsetPluggedIn() {
+                let scene = MenuScene(size: view.bounds.size)
+                scene.scaleMode = .resizeFill
+                
+                view.presentScene(scene)
+            }
         }
-    
     }
-
+    
+    func isHeadsetPluggedIn() -> Bool {
+        let route: AVAudioSessionRouteDescription = AVAudioSession.sharedInstance().currentRoute
+        for desc in route.outputs {
+            if desc.portType == AVAudioSession.Port.headphones {
+                return true
+            }
+        }
+        return false
+    }
 }
