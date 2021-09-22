01cc3dbef18bf520c86d92d34da26f7997a594a0
VitorGK <vitorgk1@gmail.com>
2021-09-22 15:57:40 -0300

    Set default SKToggleNode state to true

--- Oregano/Classes/SKToggleNode.swift
+++ Oregano/Classes/SKToggleNode.swift
@@ -6,8 +6,8 @@ class SKToggleNode: SKButtonNode {
     var ttsOn: String
     var ttsOff: String
     
-    override init(tts: String, action: @escaping () -> Void = {}) {
-        activated = false
+    override init(tts: String, state: Bool = true, action: @escaping () -> Void = {}) {
+        activated = state
         ttsOn = "Desativar " + tts
         ttsOff = "Ativar " + tts
         super.init(tts: tts)
