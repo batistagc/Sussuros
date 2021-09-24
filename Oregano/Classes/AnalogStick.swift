2688f2a5d8a99f6ae29b4cb752df08511d9d26c0
VitorGK <vitorgk1@gmail.com>
2021-09-24 16:14:33 -0300

    Change Analog Stick behavior

--- Oregano/Classes/AnalogStick.swift
+++ Oregano/Classes/AnalogStick.swift
@@ -51,7 +51,7 @@ class AnalogStick {
         if isUsing {
             let vector = CGVector(dx: location.x - outline.position.x, dy: location.y - outline.position.y)
             let angle = atan2(vector.dy, vector.dx)
-            let outlineRadius: CGFloat = 192
+            let outlineRadius: CGFloat = 128
             let distanceX: CGFloat = sin(angle - CGFloat.pi/2) * outlineRadius
             let distanceY: CGFloat = cos(angle - CGFloat.pi/2) * outlineRadius
             
