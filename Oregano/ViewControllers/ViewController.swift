a05a0a3579ca4702e51c64e0232d4cf8da86ddb6
VitorGK <vitorgk1@gmail.com>
2021-09-27 17:00:16 -0300

    Change initial scene

--- Oregano/ViewControllers/ViewController.swift
+++ Oregano/ViewControllers/ViewController.swift
@@ -27,12 +27,12 @@ class ViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             var scene: SKScene
-//            if isHeadsetPluggedIn() {
-//                scene = MenuScene(size: view.bounds.size)
-//            } else {
-//                scene = HeadphonesScene(size: view.bounds.size)
-//            }
-            scene = GameScene(size: view.bounds.size)
+            if isHeadsetPluggedIn() {
+                scene = MenuScene(size: view.bounds.size)
+            } else {
+                scene = HeadphonesScene(size: view.bounds.size)
+            }
+//            scene = GameScene(size: view.bounds.size)
             scene.scaleMode = .aspectFill
             view.presentScene(scene)
             view.ignoresSiblingOrder = true
